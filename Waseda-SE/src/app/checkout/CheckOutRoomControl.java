e14622b8e734e3c464a4133372738847e690d6e8
Manchot13 <osatoj.noshi@gmail.com>
2024-07-11 16:17:53 +0900

    チェックアウトできました

---
+++ Waseda-SE/src/app/checkout/CheckOutRoomControl.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright(C) 2007-2013 National Institute of Informatics, All rights reserved.
+ */
+package app.checkout;
+
+import java.util.Date;
+
+import app.AppException;
+import app.ManagerFactory;
+import domain.payment.PaymentManager;
+import domain.payment.PaymentException;
+import domain.room.RoomManager;
+import domain.room.RoomException;
+
+/**
+ * Control class for Check-out Customer
+ * 
+ */
+public class CheckOutRoomControl {
+
+public void checkOut(String roomNumber) throws AppException {
+      try {
+         // Assign room
+         RoomManager roomManager = getRoomManager();
+         Date stayingDate = roomManager.removeCustomer(roomNumber);
+         
+         // Create payment
+         PaymentManager paymentManager = getPaymentManager();
+         paymentManager.consumePayment(stayingDate, roomNumber);
+
+      } catch (RoomException e) {
+         AppException exception = new AppException("Failed to check-out", e);
+         exception.getDetailMessages().add(e.getMessage());
+         exception.getDetailMessages().addAll(e.getDetailMessages());
+         throw exception;
+      } catch (PaymentException e) {
+         AppException exception = new AppException("Failed to check-out", e);
+         exception.getDetailMessages().add(e.getMessage());
+         exception.getDetailMessages().addAll(e.getDetailMessages());
+         throw exception;
+      }
+   }
+
+   private RoomManager getRoomManager() {
+      return ManagerFactory.getInstance().getRoomManager();
+   }
+
+   private PaymentManager getPaymentManager() {
+      return ManagerFactory.getInstance().getPaymentManager();
+   }
+}
