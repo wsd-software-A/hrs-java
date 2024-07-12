68b6be37a269446a79a283ba4b653fda77c34b9e
yoshi-zen <yoshi180705@ruri.waseda.jp>
2024-07-12 11:05:53 +0900

    コンフリクト部分解消

--- Waseda-SE/src/app/cancel/CancelRoomControl.java
+++ Waseda-SE/src/app/cancel/CancelRoomControl.java
@@ -4,8 +4,6 @@ import java.util.Date;
 
 import app.AppException;
 import app.ManagerFactory;
-import domain.payment.PaymentManager;
-import domain.payment.PaymentException;
 import domain.reservation.ReservationManager;
 import domain.reservation.ReservationException;
 import domain.room.RoomManager;
@@ -17,46 +15,24 @@ import domain.room.RoomException;
  */
 public class CancelRoomControl {
 
-   public String cancel(String reservationNumber) throws AppException {
+   public void cancel(String reservationNumber) throws AppException {
       try {
          // Permitting only one night so that change amount of availableQty is always -1
          int availableQtyOfChange = -1;
-         try {
-            
-            // Update number of available rooms
-            RoomManager roomManager = getRoomManager();
-            roomManager.deleteRoomAvailableQty(stayingDate, availableQtyOfChange);
+         // Create reservation
+         ReservationManager reservationManager = getReservationManager();
+         Date stayingDate = reservationManager.consumeReservation(reservationNumber);
 
-            // Create reservation
-            ReservationManager reservationManager = getReservationManager();
-            Date stayingDate = reservation.getStayingDate();
-
-            reservationManager.
-
-         } catch (RoomException e) {
-            AppException exception = new AppException("Failed to reserve", e);
-            exception.getDetailMessages().add(e.getMessage());
-            exception.getDetailMessages().addAll(e.getDetailMessages());
-            throw exception;
-         } catch (ReservationException e) {
-            AppException exception = new AppException("Failed to reserve", e);
-            exception.getDetailMessages().add(e.getMessage());
-            exception.getDetailMessages().addAll(e.getDetailMessages());
-            throw exception;
-         }
-      } catch (ReservationException e) {
-         AppException exception = new AppException("Failed to check-in", e);
-         exception.getDetailMessages().add(e.getMessage());
-         exception.getDetailMessages().addAll(e.getDetailMessages());
-         throw exception;
-
+         // Update number of available rooms
+         RoomManager roomManager = getRoomManager();
+         roomManager.deleteRoomAvailableQty(stayingDate, availableQtyOfChange);
       } catch (RoomException e) {
-         AppException exception = new AppException("Failed to check-in", e);
+         AppException exception = new AppException("Failed to cancel", e);
          exception.getDetailMessages().add(e.getMessage());
          exception.getDetailMessages().addAll(e.getDetailMessages());
          throw exception;
-      } catch (PaymentException e) {
-         AppException exception = new AppException("Failed to check-in", e);
+      } catch (ReservationException e) {
+         AppException exception = new AppException("Failed to cancel", e);
          exception.getDetailMessages().add(e.getMessage());
          exception.getDetailMessages().addAll(e.getDetailMessages());
          throw exception;
@@ -71,7 +47,4 @@ public class CancelRoomControl {
       return ManagerFactory.getInstance().getRoomManager();
    }
 
-   private PaymentManager getPaymentManager() {
-      return ManagerFactory.getInstance().getPaymentManager();
-   }
 }
